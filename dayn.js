962263a54821435635e20949d9b0fc81186cd3cb
YellowMinion153 <37338443+YellowMinion153@users.noreply.github.com>
2018-03-14 15:00:46 +0300

    Update dayn.js

--- dayn.js
+++ dayn.js
@@ -31,7 +31,9 @@ bot.on('message', msg => {
     if(ignoreCase.equals(msg.content, 'g!heart')){
         if(msg.author.id === '207571677851287552'){
         msg.reply('Пошел нахер пидр');
-        }else{
+            return;
+        }
+        if(ignoreCase.equals(msg.content, 'g!heart')){
         msg.channel.send({
             embed: {
                 image: {
