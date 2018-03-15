799d601c40c35aab4eb4720af98778751e1328f8
YellowMinion153 <troll.boxxd@mail.ru>
2018-03-15 18:43:27 +0300

    s

--- dayn.js
+++ dayn.js
@@ -61,10 +61,10 @@ bot.on('message', msg => {
         msg.reply('Выберите пытку');
             if(ignoreCase.equals(msg.content, 'Сломать палец')){
                 msg.channel.send('`*кричит от боли, но ничего не рассказывает миликанцам*`')
-            }
+            };
             if(ignoreCase.equals(msg.content, 'Устроить нехватку кислорода')){
                 msg.channel.send('`*задыхаясь падает и умирает*`')
-            }
+            };
         };
 
     if(ignoreCase.equals(msg.content, 'g!heart')){
