026215b23a6b0bf0b1aac349dba5e96947f11d2a
YellowMinion153 <troll.boxxd@mail.ru>
2018-03-28 12:04:40 +0300

    s

---
+++ milikanec.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+var ignoreCase = require('ignore-case');
+
+var d = 00, h = 00, m = 00;
+
+bot.on('ready', () => {
+    console.log('Go bitch');
+bot.user.setPresence({ game: { name: 'Гуков на деревьх', type: 2} });
+});
+
+bot.on('message', msg => {
+    if(ignoreCase.equals(msg.content, '')){
+    msg.reply('');
+    return;
+}
+
+    if(ignorCase.equals(msg.content, '')){
+    msg.channel.send('')}
+    return;
+}
+
+    if(ignoreCase.equals(msg.content, '')){
+    msg.channel.send('')
+    return;
+}
+    if(ignoreCase.equals(msg.content, '')){
+    msg.channel.send('')
+    return;
+}
+
+bot.login(process.env.BOT_TOKEN);
